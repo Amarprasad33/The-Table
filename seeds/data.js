2dde11890719ef252c6c2cfb7e6500a970805c49
Amar Prasad <amarprasad77354@gmail.com>
2023-03-22 23:47:34 +0530

    custom edited data

---
+++ seeds/data.js
@@ -0,0 +1,251 @@
+module.exports = [
+    {
+      name: "Butter Chicken",
+      price: 269,
+      category: "main",
+      avaliable: true,
+      description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+      image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "kadai Paneer",
+        price: 169,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "kadai Veg",
+        price: 220,
+        category: "main",
+        avaliable: false,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Aloo Gobi",
+        price: 149,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Laziz Kumb",
+        price: 230,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Dum Aloo Kashmiri",
+        price: 180,
+        category: "main",
+        avaliable: false,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Babycorn Hasina",
+        price: 240,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Subzi-E-Zaika",
+        price: 200,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Persian Chicken Joojeh Kebabs",
+        price: 279,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Sticky Tamarind Chicken",
+        price: 299,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Skinny Skillet Chicken Cordon Bleu",
+        price: 359,
+        category: "main",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Honey Bourbon Chicken Wings",
+        price: 266,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Falafel",
+        price: 378,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Spicy Tuna Fish-Cakes",
+        price: 266,
+        category: "starter",
+        avaliable: false,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Special Zaika-e-Zafran",
+        price: 260,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Mayo Prawn",
+        price: 266,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Pan Fried Chilly Fish",
+        price: 320,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Classic Chicken Spring Roll",
+        price: 180,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Jhinga Roswali Kebab",
+        price: 266,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Tandoori Promret",
+        price: 320,
+        category: "starter",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Love Martini",
+        price: 899,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Coconut Mojito",
+        price: 299,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Holiday White Wine Sangria",
+        price: 899,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Cappy Lemon",
+        price: 399,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Masala Lassi",
+        price: 299,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Virgin Mojito",
+        price: 199,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Blue Mojito",
+        price: 399,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Iced Cofee",
+        price: 499,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Chivas Regal",
+        price: 1299,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Jacks Daniel's",
+        price: 1699,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        name: "Krombacher",
+        price: 1279,
+        category: "beverage",
+        avaliable: true,
+        description: "Grilled chicken in a luscious tomato-based spiced curry sauce.",
+        image: "https://images.pexels.com/photos/7625056/pexels-photo-7625056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+
+  ]
